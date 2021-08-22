b87c7fc08870d1174142f09e5d115dec1a6936bd
userpavankumar <pavankumartidke12345@gmail.com>
2021-08-22 14:46:46 +0530

    testing

--- admin/controller/php/signup.php
+++ admin/controller/php/signup.php
@@ -1,37 +1,43 @@
 
 <?php
 
+use MongoDB\Exception\Exception;
+
 require '../../../vendor/autoload.php';
 
-$con = new MongoDB\Client( 'mongodb://143.244.135.9:27017' );
+$con = new MongoDB\Client( 'mongod://143.244.135.9:27017' );
 $db = $con->php_mongo; 
 
+try {
 
-if(isset($_POST['ad_signup'])) {
-    $collection = $db->admin;
+    if(isset($_POST['ad_signup'])) {
+        $collection = $db->admin;
 
-    $pass = $_POST['pass'];
-    $hash = password_hash( $pass, PASSWORD_DEFAULT );
-    $a_unid = strval(rand());
-    $pendingDoc_ids = [];
-    $doc_ids = [];
-    $pat_ids = [];
+        $pass = $_POST['pass'];
+        $hash = password_hash( $pass, PASSWORD_DEFAULT );
+        $a_unid = strval(rand());
+        $pendingDoc_ids = [];
+        $doc_ids = [];
+        $pat_ids = [];
 
-    $collection->insertOne( [
-        'a_unid' => $a_unid,
-        'fname' => $_POST['fname'], 
-        'username' => $_POST['uname'], 
-        'email' => $_POST['email'], 
-        'password' => $hash, 
-        'profile_image' => '',
-        'pendingDoc_ids' => $pendingDoc_ids,
-        'pat_ids' => $pat_ids,
-        'doc_ids' => $doc_ids,
-    ] );
-    
-    header('location: http://143.244.135.9/s/admin/index?login=now');
-    // echo 'Account created success';
+        $collection->insertOne( [
+            'a_unid' => $a_unid,
+            'fname' => $_POST['fname'], 
+            'username' => $_POST['uname'], 
+            'email' => $_POST['email'], 
+            'password' => $hash, 
+            'profile_image' => '',
+            'pendingDoc_ids' => $pendingDoc_ids,
+            'pat_ids' => $pat_ids,
+            'doc_ids' => $doc_ids,
+        ] );
+        
+        header('location: http://143.244.135.9/s/admin/index?login=now');
+        // echo 'Account created success';
 
+    }
+} catch(Exception $e) {
+    print_r($e);
 }
 
 
