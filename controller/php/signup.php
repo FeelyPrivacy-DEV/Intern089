b3b116707915c485d2cdfd7b03ad99398578c205
userpavankumar <pavankumartidke12345@gmail.com>
2021-08-27 12:04:57 +0530

    server config

--- controller/php/signup.php
+++ controller/php/signup.php
@@ -1,9 +1,7 @@
 <?php
 
-// namespace App\Http\Controllers;
 require '../../vendor/autoload.php';
-use Illuminate\Http\Request;
-use Magic;
+
 $con = new MongoDB\Client( 'mongodb://test.feelyprivacy.com:27017' );
 $db = $con->php_mongo;
 
@@ -14,128 +12,95 @@ try {
 
         $email = $_POST['email'];
 
-        // class UserController extends Controller {
-        //     function signup( Request $request ) {
+        $data = array(
+            'secret' => '0xd36232060c2727eDE97A8A9F6c61ffa636779cb2',
+            'response' => $_POST['h-captcha-response']
+        );
+        $verify = curl_init();
+        curl_setopt( $verify, CURLOPT_URL, 'https://hcaptcha.com/siteverify' );
+        curl_setopt( $verify, CURLOPT_POST, true );
+        curl_setopt( $verify, CURLOPT_POSTFIELDS, http_build_query( $data ) );
+        curl_setopt( $verify, CURLOPT_RETURNTRANSFER, true );
+        $response = curl_exec( $verify );
+        var_dump( $response );
+        $responseData = json_decode( $response );
+        if ( $responseData->success ) {
+            $pass = $_POST['pass'];
+            $datetime = ( Object )[];
+            $hash = password_hash( $pass, PASSWORD_DEFAULT );
+            $d_unid = strval( rand() );
+            $p_unid = [];
+            $gen_info = [];
 
-        //         $did_token = $request->bearerToken();
+            $collection->insertOne( [
+                'd_unid' => $d_unid,
+                'p_unid' => $p_unid,
+                'password' =>$hash,
+                'fname' =>$_POST['fname'],
+                'sname' =>$_POST['sname'],
+                'email' =>$_POST['email'],
+                'gen_info' => [
+                    'phone_no' => '',
+                    'gender' => '',
+                    'DOB' => '',
+                    'biography' => '',
+                    'member_since' => date( 'Y-m-d' )
+                ],
+                'clinic_info' => [
+                    'clinic_name' => '',
+                    'clinic_addrs' => '',
+                    'clinic_image' => [],
+                ],
+                'contact_detail' => [
+                    'addressLine' => '',
+                    'clinic_addrs' => '',
+                    'city' => '',
+                    'state' => '',
+                    'country' => '',
+                    'postal_code' => '',
+                ],
+                'servicesAndSpec' => [
+                    'services' => '',
+                    'spec' => '',
+                ],
+                'education' => [
+                    'degree' => [],
+                    'college' => [],
+                    'year_of_comp' => [],
+                ],
+                'experience' => [
+                    'hospital_name' => [],
+                    'hos_from' => [],
+                    'hos_to' => [],
+                    'designation' => [],
+                ],
+                'awards' => [
+                    'aw_name' => [],
+                    'aw_year' => [],
+                ],
+                'memberships' => [],
+                'reg_name' => [],
+                'custom_price'=> '',
+                'profile_image'=> '',
+                'approved'=> false,
+                'login_able'=> false,
+                'total_earn'=> '',
+                'datetime'=> $datetime
+            ] );
 
-        //         if ( $did_token == null ) {
-        //             // DIDT is missing from the original HTTP request header.
-        //             // You can handle this by remapping it to your application error.
-        //         }
+            $collection = $db->admin;
+            $collection->updateOne(
+                ['a_unid' => '1123498786'],
+                ['$push' =>['pendingDoc_ids' => $d_unid]]
+            );
 
-        //         try {
-        //             // Validate the did token
-        //             Magic::token()->validate( $did_token );
-        //             $issuer = Magic::token()->get_issuer( $did_token );
-        //             $user_meta = Magic::user()->get_metadata_by_issuer( $issuer );
-        //         } catch ( Throwable $e ) {
-        //             // DIDT is malformed.
-        //             // You can handle this by remapping it to your application error.
-        //             report( $e );
-        //             return false;
-        //         }
+            header( 'location: http://test.feelyprivacy.com/s/index?login=now' );
+            // echo 'Account created success';
+        }
+        else {
+            header( 'location: http://test.feelyprivacy.com/s/index?c=e' );
+        }
 
-        //         if ( $user_meta->data['email'] != $email ) {
-        //             // Unauthorized sign-up.
-        //             // You can handle this by remapping it to your application error.
-        //         }
-
-        //         // Call your application logic to save the user.
-        //         $logic->user->add( $name, $email, $issuer );
-
-        //         // Final step, call your application logic to sign up and/or log in the user.
-
-        //     }
-        // }
-
-        // $data = array(
-        //     'secret' => '0xd36232060c2727eDE97A8A9F6c61ffa636779cb2',
-        //     'response' => $_POST['h-captcha-response']
-        // );
-        // $verify = curl_init();
-        // curl_setopt( $verify, CURLOPT_URL, 'https://hcaptcha.com/siteverify' );
-        // curl_setopt( $verify, CURLOPT_POST, true );
-        // curl_setopt( $verify, CURLOPT_POSTFIELDS, http_build_query( $data ) );
-        // curl_setopt( $verify, CURLOPT_RETURNTRANSFER, true );
-        // $response = curl_exec( $verify );
-        // // var_dump( $response );
-        // $responseData = json_decode( $response );
-        // if ( $responseData->success ) {
-        //     $pass = $_POST['pass'];
-        //     $datetime = ( Object )[];
-        //     $hash = password_hash( $pass, PASSWORD_DEFAULT );
-        //     $d_unid = strval( rand() );
-        //     $p_unid = [];
-        //     $gen_info = [];
-
-        //     $collection->insertOne( [
-        //         'd_unid' => $d_unid,
-        //         'p_unid' => $p_unid,
-        //         'password' =>$hash,
-        //         'fname' =>$_POST['fname'],
-        //         'sname' =>$_POST['sname'],
-        //         'email' =>$_POST['email'],
-        //         'gen_info' => [
-        //             'phone_no' => '',
-        //             'gender' => '',
-        //             'DOB' => '',
-        //             'biography' => '',
-        //             'member_since' => date( 'Y-m-d' )
-        //         ],
-        //         'clinic_info' => [
-        //             'clinic_name' => '',
-        //             'clinic_addrs' => '',
-        //             'clinic_image' => [],
-        //         ],
-        //         'contact_detail' => [
-        //             'addressLine' => '',
-        //             'clinic_addrs' => '',
-        //             'city' => '',
-        //             'state' => '',
-        //             'country' => '',
-        //             'postal_code' => '',
-        //         ],
-        //         'servicesAndSpec' => [
-        //             'services' => '',
-        //             'spec' => '',
-        //         ],
-        //         'education' => [
-        //             'degree' => [],
-        //             'college' => [],
-        //             'year_of_comp' => [],
-        //         ],
-        //         'experience' => [
-        //             'hospital_name' => [],
-        //             'hos_from' => [],
-        //             'hos_to' => [],
-        //             'designation' => [],
-        //         ],
-        //         'awards' => [
-        //             'aw_name' => [],
-        //             'aw_year' => [],
-        //         ],
-        //         'memberships' => [],
-        //         'reg_name' => [],
-        //         'custom_price'=> '',
-        //         'profile_image'=> '',
-        //         'approved'=> false,
-        //         'login_able'=> false,
-        //         'total_earn'=> '',
-        //         'datetime'=> $datetime
-        //     ] );
-
-        //     $collection = $db->admin;
-        //     $collection->updateOne(
-        //         ['a_unid' => '1123498786'],
-        //         ['$push' =>['pendingDoc_ids' => $d_unid]]
-        // );
-
-        //     header( 'location: http://test.feelyprivacy.com/s/d?login=now' );
-        //     // echo 'Account created success';
-        // }
-
-        header( 'location: http://test.feelyprivacy.com/s/index?c=e' );
         // print_r( $responseData );
 
     } else if ( isset( $_POST['employee_signup'] ) ) {
